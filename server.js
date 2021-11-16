7347af66d515437bf2cf5b1705e379c860b28997
rykesz <adfourretout@hotmail.fr>
2021-11-16 18:51:13 +0100

    fix CORS

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const newsletterService = require('./services/newsletterService')
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://going-nuclear.netlify.app');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
