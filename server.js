17b3174c44345a559c67f92f2a519e065e484061
rykesz <adfourretout@hotmail.fr>
2021-11-18 08:37:23 +0100

    fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const newsletterService = require('./services/newsletterService')
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', process.env.FRONT_END_URL);
+    res.setHeader('Access-Control-Allow-Origin', 'https://going-nuclear-43tjm.ondigitalocean.app');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
