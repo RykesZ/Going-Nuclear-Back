3c4a50236a184e4872c22b4bb077a3a8a071844c
rykesz <adfourretout@hotmail.fr>
2021-11-19 15:04:19 +0100

    CORS better fix

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const newsletterService = require('./services/newsletterService')
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://going-nuclear-43tjm.ondigitalocean.app' && 'https://going-nuclear.fr' && 'http://localhost:3000');
+    const allowedOrigins = ['https://going-nuclear-43tjm.ondigitalocean.app', 'https://going-nuclear.fr', 'http://localhost:3000'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    }
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
